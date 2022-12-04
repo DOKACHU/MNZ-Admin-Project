a509ba01eadab582e0cafe5d9dc756a44fbf6428
김민수 <gimm14851@gmail.com>
2022-12-04 22:53:32 +0900

    feat: add vite config path

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+import Test from './Test';
+
+// in mocking
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Test />
+  </React.StrictMode>
+);
