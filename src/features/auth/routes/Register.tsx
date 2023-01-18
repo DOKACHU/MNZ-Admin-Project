d87050c182688eef3284756abf2ddf9a51f7f8de
김민수 <gimm14851@gmail.com>
2023-01-18 23:23:55 +0900

    feat: Add Register Logic

--- src/features/auth/routes/Register.tsx
+++ src/features/auth/routes/Register.tsx
@@ -1,20 +1,31 @@
+/* eslint-disable import/no-cycle */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { Grid, TextField, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { AuthLayout } from '../../../layouts';
+import { RegisterDTO } from '../api/register';
+import { useRegister } from '../../../lib/auth';
 
 export default function Register() {
   const { control, handleSubmit, reset } = useForm<any>();
   const navigate = useNavigate();
+  const { mutateAsync } = useRegister();
 
   const handleRoute = () => {
     navigate('/auth/login');
   };
 
+  const onSubmit = async (data: RegisterDTO) => {
+    console.log({ data });
+    // const result = await mutateAsync({ data });
+    // reset(result as any);
+    // handleClose();
+  };
+
   return (
-    <AuthLayout title="회원가입">
+    <AuthLayout title="회원가입" onSubmit={handleSubmit(onSubmit)}>
       <Grid item xs={12}>
         <Controller
           name="email"
@@ -40,7 +51,9 @@ export default function Register() {
         />
       </Grid>
       <Grid item xs={12}>
-        <Button variant="contained">가입 완료</Button>
+        <Button type="submit" variant="contained">
+          가입 완료
+        </Button>
         <Button onClick={handleRoute}> 로그인으로 이동 </Button>
       </Grid>
     </AuthLayout>
